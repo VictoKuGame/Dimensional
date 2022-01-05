3d4e8281642dc9992cbb48a61f8b117ec3cccbfb
VictoKu1 <kushnir.victor@gmail.com>
2022-01-05 07:59:34 +0200

    .

--- DimensionalMazeProj/Assets/Scripts/LoaderTextControl.cs
+++ DimensionalMazeProj/Assets/Scripts/LoaderTextControl.cs
@@ -21,7 +21,7 @@ public class LoaderTextControl : MonoBehaviour
         }
         else
         {
-            textValue1 = "Reloading Level: ";
+            textValue1 = "Loading Level: ";
         }
         textValue1 += GameControlManage.level + ".\n";
         textValue2 = "\nRemember: The size of the map,the nubmer of enemies and their strength will upgrade as you complete every level. " + ((GameControlManage.level >= 5) ? "\n" : "") + "So it will just become harder to you (and to your PC to render all this mess) from a level to level.";
